d47ed632c1d8543e6881f620fc763086728ef550
nitanssj <nitanssj@amdocs.com>
2024-05-05 17:45:08 +0530

    Stackup Bounty: Building a Customized RAG Model working code with snapshots

--- key_param.py
+++ key_param.py
@@ -1,2 +1,2 @@
-openai_api_key = "sk-proj-0ayIxGNtQYw0JtBAew7tT3BlbkFJN4urEMUshPXuhiSMiBrC"
-MONGO_URI = "mongodb+srv://nitansshu:JAINni1ta2@cluster0.obncbgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+openai_api_key = "ENTER_OPENAI_API_KEY_HERE"
+MONGO_URI = "ENTER_MONGODB_URI_HERE"
